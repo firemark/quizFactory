adfef9c0624c0e257625418af7a305f5751a1c7c
Firemark <tiramo@o2.pl>
2013-10-12 21:05:18 +0200

    and form support + checkbox support

--- quizfactory/static/js/answer_types.js
+++ quizfactory/static/js/answer_types.js
@@ -3,6 +3,7 @@ var AnsTypeView = Backbone.View.extend({
         _.bindAll(this, 'render');
         this.template_begin = _.template(args.template_begin);
         this.template_finish = _.template(args.template_finish);
+        this.generate_choice = args.generate_choice;
     },
     render: function(answers, choice, end){
         var template = end?this.template_finish:this.template_begin;
@@ -22,22 +23,40 @@ var AnsTypeView = Backbone.View.extend({
 
 var AnsTypeViews = {
     radio: new AnsTypeView({
+        template_begin: "<li class='list-group-item <%-choice==hash?'active':''%>'>\
+        <input type='radio' name='ans' <%-choice==hash?'checked':''%> value='<%=hash%>' /> <%-text%>\
+        </li>",
         template_finish: "<li class='list-group-item'>\
-        <input type='radio' <%-choice==hash?'checked':''%> value='<%=hash%>' /> <%-text%>\
+        <input type='radio' name='ans' disabled='disabled' <%-choice==hash?'checked':''%> value='<%=hash%>' /> <%-text%>\
         <% if(is_correct){ %>\
             <span class='label label-default label-success'>OK</span>\
         <% }else if(choice==hash){ %>\
              <span class='label label-default label-danger'>WRONG</span>\
         <% } %>\
         </li>",
-        template_begin: "<li class='list-group-item <%-choice==hash?'active':''%>'>\
-        <input type='radio' <%-choice==hash?'checked':''%> value='<%=hash%>' /> <%-text%>\
-        </li>"
+        generate_choice: function(form){
+            return form.find("input:checked").attr('value');         
+        }
     }),
     checkbox: new AnsTypeView({
-        template_begin: "<li class='list-group-item <%-choice==hash?'active':''%>'>\
-        <input type='checkbox' <%-choice==hash?'checked':''%> name='<%=hash%>' /> <%-text%>\
+        template_begin: "<li class='list-group-item <%-choice.indexOf(hash)>=0?'active':''%>'>\
+        <input type='checkbox' <%-choice.indexOf(hash)>=0?'checked':''%> name='ans' value='<%=hash%>' /> <%-text%>\
         </li>",
-        template_finish: ""
+        template_finish: "<li class='list-group-item'>\
+        <input type='checkbox' disabled='disabled' <%-choice.indexOf(hash)>=0?'checked':''%> value='<%=hash%>' /> <%-text%>\
+        <% if(is_correct){ %>\
+            <span class='label label-default label-success'>OK</span>\
+        <% }else if(choice.indexOf(hash)>=0){ %>\
+             <span class='label label-default label-danger'>WRONG</span>\
+        <% } %>\
+        </li>",
+        generate_choice: function(form){
+            var choice = [];
+            form.find("input:checked").each(function(index, el){
+                choice.push(el.value);
+            })
+
+            return choice;
+        }
     })
 };
