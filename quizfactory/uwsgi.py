a95e9c26345a552a6e13b8eb088d5fc42dca8eb6
Firemark <tiramo@o2.pl>
2013-11-07 12:05:33 +0100

    add getenv to uwsgi

--- quizfactory/uwsgi.py
+++ quizfactory/uwsgi.py
@@ -1,5 +1,9 @@
+import os
+from importlib import import_module
+
 from quizfactory import app
-import quizfactory.settings
 from quizfactory.__main__ import setup
 
-setup(quizfactory.settings)
+module_name = os.getenv("QUIZFACTORY_SETTINGS", "quizfactory.settings")
+module = import_module(module_name)
+setup(module)
